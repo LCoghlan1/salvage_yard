742ea81bb1a18a8a0fc0de656cd589a9bbf2ef0f
Ubuntu <ubuntu@ip-172-31-4-129.ec2.internal>
2020-12-24 11:07:53 +0000

    create items controller and style index page. create user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
+  
+  resources :items
+  root 'static_pages#home'
+  
+  get 'about', to: 'static_pages#about'
+  
+  get 'help', to: 'static_pages#help'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
