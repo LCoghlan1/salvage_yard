0623d8809d327ebdc59b6ba098b5123832d41657
Ubuntu <ubuntu@ip-172-31-4-129.ec2.internal>
2020-12-24 13:29:54 +0000

    create users amd add secure password for user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_24_122630) do
+ActiveRecord::Schema.define(version: 2020_12_24_132547) do
 
   create_table "items", force: :cascade do |t|
     t.string "title"
@@ -27,6 +27,15 @@ ActiveRecord::Schema.define(version: 2020_12_24_122630) do
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
