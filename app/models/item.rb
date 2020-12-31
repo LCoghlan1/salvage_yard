b3c720808685ee009ea8a97f30450656240a7231
Ubuntu <ubuntu@ip-172-31-4-129.ec2.internal>
2020-12-31 21:39:17 +0000

    style fixes

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,6 @@
 class Item < ApplicationRecord
+    validates :title, presence: true, length: { minimum: 6, maximum: 100 }
+    validates :description, presence: true, length: {minimum: 10, maximum: 500 }
+    validates :price, presence: true
+    validates :image_url, presence: true
 end
