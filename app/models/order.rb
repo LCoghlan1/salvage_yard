4bd6aeacbeec4274528c45740446ba92eb910e7a
Ubuntu <ubuntu@ip-172-31-4-129.ec2.internal>
2021-01-01 13:41:39 +0000

    style and view fixes

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ApplicationRecord
   
   belongs_to :user
-  has_many :orderitems
+  has_many :orderitems, dependent: :delete_all
 
   def order_params
         params.require(:order).permit( :order_date, :user_id, :status)
